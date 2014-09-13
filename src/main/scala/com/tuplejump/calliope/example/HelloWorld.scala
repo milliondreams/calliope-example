90c4e79d66552a60b951dfd6af1ca47c72470c91
Rohit Rai <rohit@tuplejump.com>
2014-09-13 15:09:11 +0530

    Updating to use 1.1.0-CTP

--- src/main/scala/com/tuplejump/calliope/example/HelloWorld.scala
+++ src/main/scala/com/tuplejump/calliope/example/HelloWorld.scala
@@ -5,7 +5,7 @@ import java.nio.ByteBuffer
 import com.datastax.driver.core.Row
 import com.tuplejump.calliope.CasBuilder
 import com.tuplejump.calliope.Implicits._
-import com.tuplejump.calliope.Types.{CQLRowKeyMap, CQLRowValues}
+import com.tuplejump.calliope.Types._
 import com.tuplejump.calliope.utils.RichByteBuffer._
 import org.apache.spark.{SparkConf, SparkContext}
 
@@ -17,10 +17,12 @@ object HelloWorld extends Serializable {
 
   private val writeTable = "emp_write_test"
 
+  /** Used with nativeCassandra **/
   implicit val row2employee: Row => Employee = {
     row => Employee(row.getInt("deptid"), row.getInt("empid"), row.getString("first_name"), row.getString("last_name"))
   }
 
+  /** Used with cql3SaveToCassandra **/
   implicit val employee2RowKeyMap: Employee => CQLRowKeyMap = {
     emp =>
       Map[ByteBuffer, ByteBuffer]("deptid" -> emp.deptid, "empid" -> emp.empid)
@@ -29,6 +31,11 @@ object HelloWorld extends Serializable {
   implicit val employee2RowValues: Employee => CQLRowValues = {
     emp =>
       List(emp.first_name, emp.last_name)
+  }
+
+  /** Used by cql3Cassandra **/
+  implicit val cas2Employee: (CQLRowKeyMap, CQLRowMap) => Employee = {
+    case (k, v) => Employee(k("deptid"), k("empid"), v("first_name"), v("last_name"))
   }
 
 
@@ -50,28 +57,45 @@ object HelloWorld extends Serializable {
 
     val sc = new SparkContext(conf)
 
-    val casRead = CasBuilder.native.withColumnFamilyAndKeyColumns(keyspace, readTable, "deptid").onHost(host).mergeRangesInMultiRangeSplit(256)
+    /** NATIVE CQL DEMO **/
+    /* val inputCql: String = s"SELECT * FROM ${keyspace}.${readTable} WHERE token(deptid) > ? AND token(deptid) <= ? AND empid = 105 ALLOW FILTERING"
 
-    println("Reading Employees: ")
+    val casRead = CasBuilder.native.withColumnFamilyAndQuery(keyspace, readTable, inputCql).onHost(host).mergeRangesInMultiRangeSplit(256)
+
+    println("Reading Employees Using Native CQL: ")
 
     val elist = sc.nativeCassandra[Employee](casRead).collect()
 
+    println(elist)
+
     elist.map(println)
+
+    Thread.sleep(10000) */
+
+    /** CQL3 on Thrift **/
+    val casReadc = CasBuilder.cql3.withColumnFamily(keyspace, readTable).onHost(host).where("empid = 105")
+
+    println("Reading Employees Using CQL on Thrift: ")
+
+    val elistc = sc.cql3Cassandra[Employee](casReadc).collect()
+
+    println(elistc)
+
+    elistc.map(println)
 
     Thread.sleep(10000)
 
-    val elist2 = List(Employee(1001, 10011, "Arthur", "Bentley"), Employee(1001, 10012, "Andrew", "Reeds"), Employee(1001, 10013, "James", "Courtney"), Employee(1001, 10014, "Richard", "Haley"), Employee(1002, 10021, "Clarence", "Brown"))
+    println("Writing TO Cassandra: ")
+
+    /* val elist2 = List(Employee(1001, 10011, "Arthur", "Bentley"), Employee(1001, 10012, "Andrew", "Reeds"), Employee(1001, 10013, "James", "Courtney"), Employee(1001, 10014, "Richard", "Haley"), Employee(1002, 10021, "Clarence", "Brown"))
 
     val empRdd = sc.parallelize(elist2)
 
     val casWrite = CasBuilder.cql3.withColumnFamily(keyspace, writeTable).onHost(host).saveWithQuery(s"UPDATE ${keyspace}.${writeTable} set first_name = ?, last_name = ?")
 
-
-    println("Writing Employees: ")
-
     empRdd.cql3SaveToCassandra(casWrite)
 
-    Thread.sleep(10000)
+    Thread.sleep(10000) */
 
 
   }
